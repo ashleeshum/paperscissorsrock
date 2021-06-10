fb9f5c453a6979b96047940a8bf84be1d433e63f
ashleeshum <66580880+ashleeshum@users.noreply.github.com>
2021-06-10 19:44:02 +1200

    Update paper scissors rock v1.py

--- paper scissors rock v1.py
+++ paper scissors rock v1.py
@@ -4,14 +4,16 @@ import random
 valid_choices = ["p","s","r"]
 # linking the p, s and r to the description
 psr_map = {"p" : "paper", "s" : "scissors", "r" : "rock"}
+# made an undefined list in order to not use while true 
+user_play = ""
 # main while loop for whole game
-while True:
+while user_play == "":
   # asking user if they want to play the game
   user_answer = input("do you want to play a game of paper scissors rock? type y for yes and n for no: ")
   # if statement to run the game if user says yes
   if user_answer == "y":
     # while loop for users choice if they didn't enter valid input
-    while True:
+    while user_answer == "y":
       # asking user to choose either p s or r
      user_choice = input("choose rock, paper or scissors. type r for rock, p for paper and s for scissors: ")
      # if statement to validate user input
